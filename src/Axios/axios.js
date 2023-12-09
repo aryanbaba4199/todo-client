d1caa7184c490f01edfcb8584a461d3b74b8fd62
Aryan Dubey <aryanbaba@gmail.com>
2023-12-09 10:11:10 +0530

    Deplyement setting

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -3,6 +3,6 @@ import axios from "axios"
 
 // --------Connecting to Backend --------
 const instance = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"http://todo-server-raushan-kumars-projects-60d040ea.vercel.app/api"
 })
 export default instance;
