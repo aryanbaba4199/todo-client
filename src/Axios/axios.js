04e25d1e213b018c3a4f6c7ba635c3415ee93abb
Aryan Dubey <aryanbaba@gmail.com>
2023-12-09 10:21:52 +0530

    ef

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -3,6 +3,6 @@ import axios from "axios"
 
 // --------Connecting to Backend --------
 const instance = axios.create({
-    baseURL:"http://todo-server-raushan-kumars-projects-60d040ea.vercel.app/api"
+    baseURL:"https://todo-server-coral.vercel.app/api"
 })
 export default instance;
